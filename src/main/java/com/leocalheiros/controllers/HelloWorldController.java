fb2deaa2fd79c3fd4a10d173d4a3c5f1082f005c
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2025-01-16 12:37:03 -0300

    refact: change file directory

---
+++ src/main/java/com/leocalheiros/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.leocalheiros.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String getHelloWorld(){
+        return "Hello World";
+    }
+}
