6d62badbfd92d13f7a30187dcae6186a467b19ba
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2025-01-16 10:53:59 -0300

    feat: upload basic structure

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,10 @@
+﻿import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ExampleTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
